eaf4dbe709ea37b142c1577f2ca46c53f16be895
pragyajhala121 <73744592+pragyajhala121@users.noreply.github.com>
2020-12-13 15:24:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function getp1(){
+    var inputs=[];
+    for(var i=1;i<=6;i++){
+        inputs.push(document.getElementById("para1_input_box_"+i).value);
+        console.log(inputs);
+    }
+    var length_of_the_inputs=inputs.length;
+    for(var j=0;j<inputs.length;j++){
+        inputs.join(". ")
+        document.getElementById("display_Paragraph1").innerHTML=inputs.join(". ");
+        console.log(inputs);
+
+    }
+    
+
+}
+function getp2(){
+    var inputs=[];
+    for(var i=1;i<=6;i++){
+        inputs.push(document.getElementById("para2_input_box_"+i).value);
+        console.log(inputs);
+    }
+    var length_of_the_inputs=inputs.length;
+    for(var j=0;j<inputs.length;j++){
+        inputs.join(". ")
+        document.getElementById("display_Paragraph2").innerHTML=inputs.join(". ");
+        console.log(inputs);
+    }
+    
+}
